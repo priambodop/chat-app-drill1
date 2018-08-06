2a473206dce2c56ba96fdb6f7df177b86fda1394
priambodop <priambodop>
2018-08-06 11:38:58 +0700

    Added connect and disconnect event to socket.io

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname + '/../public');
 const app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log('Server is listening to 3000...');
 });
